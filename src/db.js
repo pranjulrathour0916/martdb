a6a9a9a7bce2a0dfa7ecabcfdaa4f137214842a4
pranjulrathour0916 <pranjalrathore9838@gmail.com>
2026-03-16 09:03:28 +0530

    pool change

--- src/db.js
+++ src/db.js
@@ -3,11 +3,9 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
-
 export default pool;
